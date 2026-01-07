5092483abb5dbaf6ae50aa1550f62bec4efbcbcd
José Luis Merino <jose-luis.merino@unirioja.es>
2026-01-07 12:38:24 +0100

    Config: ajustar site y base para despliegue en GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://jomeridi.github.io",
+  base: "/portal-inves",
   integrations: [react()],
   image: {
     domains: ["images.unsplash.com"],
